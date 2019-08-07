19999ea83b9adee1d8a8b3c2523e6105687581e6
Medet <MedetTl@halykbank.kz>
2019-08-07 13:19:26 +0600

    refactor

--- index.js
+++ index.js
@@ -5,18 +5,11 @@ var http = require('http'),
 // Create a proxy server with custom application logic
 //
 var proxy = httpProxy.createProxyServer({});
-
-//
-// Create your custom server and just call `proxy.web()` to proxy
-// a web request to the target passed in the options
-// also you can use `proxy.ws()` to proxy a websockets request
-//
 var server = http.createServer(function(req, res) {
   // You can define here your custom logic to handle the request
   // and then proxy the request.
-  req.
   proxy.web(req, res, { target: 'https://github.com' });
 });
 
-console.log("listening on port 80")
-server.listen(80);
+console.log("listening on port 8080")
+server.listen(8080);
